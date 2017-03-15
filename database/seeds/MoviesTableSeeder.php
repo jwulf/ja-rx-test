b84c52353bbebd02126d6afafb9131974c1c0764
Jess Archer <jess@jessarcher.com>
2017-03-15 16:38:49 +1000

    Ref #00007: Update seeders

--- database/seeds/MoviesTableSeeder.php
+++ database/seeds/MoviesTableSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Actor;
+use App\Movie;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
 class MoviesTableSeeder extends Seeder
@@ -11,6 +14,14 @@ class MoviesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Movie::class, 50)->create();
+        $actors = Actor::all();
+
+        $faker = Faker::create();
+
+        factory(Movie::class, 50)->create()->each(function ($movie) use ($actors, $faker) {
+            foreach ($actors->random(rand(5, 10)) as $randomActor) {
+                $movie->actors()->attach($randomActor->id, ['character' => $faker->name]);
+            }
+        });
     }
 }
